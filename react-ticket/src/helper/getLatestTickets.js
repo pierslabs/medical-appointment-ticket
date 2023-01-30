8c05b2d999f09362d06507f03a4ab0fb226b0bf0
pierslabs <pedrolosasp@gmail.com>
2023-01-30 20:08:59 +0100

    renamed front && back dir

--- react-ticket/src/helper/getLatestTickets.js
+++
@@ -1,6 +0,0 @@
-export const getLatestTickets = async () => {
-  const res = await fetch('http://localhost:4000/lastTickets');
-  const data = await res.json();
-
-  return data;
-};
--- react-ticket/src/helper/getUserStorage.js
+++
@@ -1,6 +0,0 @@
-export const getUserStorage = () => {
-  return {
-    desktop: localStorage.getItem('desktop'),
-    agent: localStorage.getItem('agent'),
-  };
-};
